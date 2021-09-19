5dc268a06085b10d0e808525672f47917170f803
raul4916 <raul4916@users.noreply.github.com>
2021-09-19 11:43:34 -0300

    updating a few with the new  material UI

--- src/MainLayout/NavBar.tsx
+++ src/MainLayout/NavBar.tsx
@@ -1,25 +1,48 @@
-import {Component, CSSProperties} from "react";
-import "../less/main.css";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 
-type Props = {
-    styleChange: CSSProperties
-}
+export const NavBar = () => {
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <ThemeProvider
+                theme={createTheme({
+                    palette: {
+                        primary: {
+                            main: '#16181D',
+                        },
+                        secondary: {
+                            main: '#3a3f4d',
+                        },
+                    }
+                })}
+            >
+                < AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{mr: 2}}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            SM64 Fantasy
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </ThemeProvider>
+        </Box>
 
-export const NavBar = (props: Props) => {
-        return (
-            <ul className="nav navbar nav-justified color-check" >
-                <li className="nav-item dark-btn">
-                    <div className="text-white dark-btn" aria-current="page">League Leaders</div>
-                </li>
-                <li className="nav-item dark-btn">
-                    <div className="text-white dark-btn" aria-current="page">League Leaders</div>
-                </li>
-                <li className="nav-item dark-btn">
-                    <div className="text-white dark-btn" aria-current="page">Statistics</div>
-                </li>
-                <li className="nav-item  dark-btn">
-                    <div className="text-white dark-btn" aria-current="page">Speedrunner Statistics</div>
-                </li>
-            </ul>
-        );
+    )
+        ;
 }
